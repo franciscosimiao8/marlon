7ef0eea682c6f916fb1f53fc0135b110cc872150
Francisco Marlon <53242022+franciscosimiao8@users.noreply.github.com>
2020-06-02 01:27:05 -0400

    Add files via upload

---
+++ marlon.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+
+sudo apt install python3.8 -y
+sudo apt install python3-pip -y
+sudo apt update
+sudo apt install snap -y
+sudo apt install snap -y
+sudo snap install spotify
+sudo snap install sublime-text --classic
+sudo snap install pycharm-community --classic
+sudo snap install android-studio --classic
+sudo snap install eclipse --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install phpstorm --classic
+sudo snap install webstorm --classic
+sudo snap install clion --classic
+sudo snap install rubymine --classic
+sudo snap install code --classic
+sudo snap install node --classic
+sudo snap install skype --classic
+sudo snap install discord
+sudo snap install taskbook
+sudo snap install okular
+sudo snap install spotify
+sudo snap install obs-studio
+sudo snap install gimp
+sudo snap install vlc
+sudo add-apt-repository ppa:system76/pop -y
+sudo apt update
+sudo apt install pop-icon-theme
+
